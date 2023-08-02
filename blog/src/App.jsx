94b85b2fc5c843138ab88c659d2c5a8fc0554733
João Souza <91389107+iamjonesss@users.noreply.github.com>
2023-08-02 11:51:47 -0300

    css implementation and get info movies

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import TMDB from './TMDB'
 import MovieRow from './components/MovieRow';
-
+import FeaturedMovie from './components/FeaturedMovie';
 
 
 function App() {
   
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
   
   useEffect(() => {
     const loadAll = async () => {
@@ -15,6 +16,15 @@ function App() {
       
       let list = await TMDB.getHomeList();
       setMovieList(list);
+
+
+      // Pegando o Destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+
+      let chosenInfo = await TMDB.getMovieInfo(chosen.id, 'tv');
+      console.log(chosenInfo)
     }
     
     loadAll();
@@ -23,6 +33,11 @@ function App() {
 
   return (
     <div className='page'>
+
+      {featureData &&
+        <FeaturedMovie item={featureData} />
+      }
+
       <section className='lists'>
         {movieList.map((item, key) => (
           <MovieRow key={key} title={ item.title } items={ item.items } />
---
+++ blog/src/components/FeaturedMovie.jsx
@@ -0,0 +1,16 @@
+import './FeaturedMovie.css'
+
+export default () => {
+    return (
+        <div>
+            FILME EM DESTAQUE
+
+
+
+
+        </div>
+
+
+
+    )
+}
